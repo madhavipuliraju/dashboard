e94ba1492f7ba915a8017527048d9357ca9f485b
madhavipuliraju <madhavipuliraju@gmail.com>
2014-12-31 11:43:31 +0530

    completed 8 possiblities

--- getlabsbyinstitute.php
+++ getlabsbyinstitute.php
@@ -1,6 +1,7 @@
 <?php
 $p = $_POST["p"];//institute
 $q = $_POST["q"];//discipline
+$r = $_POST["r"];//status
 
 $con = mysqli_connect('localhost','root','root','vlabs_database');
 if (!$con) {
@@ -9,7 +10,7 @@ if (!$con) {
 
 mysqli_select_db($con,"vlabs_database");
 
-if($p!=0 && $q==0)//disply only institutes
+if($p!=0 && $q==0 && $r=='None')//disply only institutes
 {
 $sql="SELECT * FROM labs WHERE institute_id='$p'";
 $result = mysqli_query($con,$sql);
@@ -28,10 +29,10 @@ echo "</table>";
 
 
 
-else if($p==0 && $q!=0)
+else if($p==0 && $q!=0 && $r=='None')//only disciplines 
 {
 
-$sql="SELECT * FROM labs WHERE institute_id='$q'";
+$sql="SELECT * FROM labs WHERE discipline_id='$q'";
 $result = mysqli_query($con,$sql);
 $two = mysqli_num_rows($result);    
 echo "<br><br><h4>Total Number Of Labs:".$two."</h4>";
@@ -46,7 +47,30 @@ echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</
 echo "</table>";
 }
 
-else if($p!=0 && $q!=0)
+
+else if($p==0 && $q==0 && $r!='None') //only status
+{
+
+
+$sql="SELECT * FROM labs a WHERE a.status='$r'";
+$result = mysqli_query($con,$sql);
+$two = mysqli_num_rows($result);    
+echo "<br><br><h4>Total Number Of Labs:".$two."</h4>";
+echo "<br><br>";
+echo "<table border='2'>";
+echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</td><td>STATUS</td></tr>";
+      while($row = mysqli_fetch_array($result)) 
+      {  
+       echo "<tr align='left'><td>".$row['lab_id']."</td><td>" . $row['lab_name'] ."</td><td>" . $row['integration_level'] ."</td><td>" . $row['status'] ."</td></tr>";
+
+       }
+echo "</table>";
+}
+
+
+
+
+else if($p!=0 && $q!=0 && $r=='None')
 {
 
 
@@ -64,5 +88,66 @@ echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</
        }
 echo "</table>";
 }
+else if($p!=0 && $q!=0 && $r!='None')
+{
+
+
+$sql="SELECT * FROM labs a WHERE a.institute_id='$p' and a.discipline_id='$q' and a.status='$r'";
+$result = mysqli_query($con,$sql);
+$two = mysqli_num_rows($result);    
+echo "<br><br><h4>Total Number Of Labs:".$two."</h4>";
+echo "<br><br>";
+echo "<table border='2'>";
+echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</td><td>STATUS</td></tr>";
+      while($row = mysqli_fetch_array($result)) 
+      {  
+       echo "<tr align='left'><td>".$row['lab_id']."</td><td>" . $row['lab_name'] ."</td><td>" . $row['integration_level'] ."</td><td>" . $row['status'] ."</td></tr>";
+
+       }
+echo "</table>";
+}
+
+
+else if($p!=0 && $q==0 && $r!='None')
+{
+
+
+$sql="SELECT * FROM labs a WHERE a.institute_id='$p' and a.status='$r'";
+$result = mysqli_query($con,$sql);
+$two = mysqli_num_rows($result);    
+echo "<br><br><h4>Total Number Of Labs:".$two."</h4>";
+echo "<br><br>";
+echo "<table border='2'>";
+echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</td><td>STATUS</td></tr>";
+      while($row = mysqli_fetch_array($result)) 
+      {  
+       echo "<tr align='left'><td>".$row['lab_id']."</td><td>" . $row['lab_name'] ."</td><td>" . $row['integration_level'] ."</td><td>" . $row['status'] ."</td></tr>";
+
+       }
+echo "</table>";
+}
+
+else if($p==0 && $q!=0 && $r!='None')
+{
+
+
+$sql="SELECT * FROM labs a WHERE a.discipline_id='$q' and a.status='$r'";
+$result = mysqli_query($con,$sql);
+$two = mysqli_num_rows($result);    
+echo "<br><br><h4>Total Number Of Labs:".$two."</h4>";
+echo "<br><br>";
+echo "<table border='2'>";
+echo "<tr align='center'><td>LAB ID</td><td>LAB NAME</td><td>INTEGRATION LEVEL</td><td>STATUS</td></tr>";
+      while($row = mysqli_fetch_array($result)) 
+      {  
+       echo "<tr align='left'><td>".$row['lab_id']."</td><td>" . $row['lab_name'] ."</td><td>" . $row['integration_level'] ."</td><td>" . $row['status'] ."</td></tr>";
+
+       }
+echo "</table>";
+}
+
+
+
+
 mysqli_close($con);
 ?> 
